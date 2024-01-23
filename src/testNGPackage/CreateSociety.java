b1c3931d99867c31c9a31d9594f9f2c7dfafed26
Sandeep Yadav <er,sandeep777@gmail.com>
2024-01-23 16:09:16 +0530

    new file added

---
+++ src/testNGPackage/CreateSociety.java
@@ -0,0 +1,11 @@
+package testNGPackage;
+
+import org.testng.annotations.Test;
+
+public class CreateSociety {
+	@Test
+	public void createsociety() {
+		System.out.println("New Changes ");
+	}
+
+}
